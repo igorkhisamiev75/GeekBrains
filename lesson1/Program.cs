511e19ce1ac29875161a34d3f39437bfa516d308
igorkhisamiev75 <khisamievigor@gmail.com>
2021-01-18 11:57:31 +0300

    lesson1
    Первый урок. Ввод и вывод данных.  Примеры комментария.  Интерполяция. Конкатенация строк+

---
+++ lesson1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+//using System.Data;
+
+namespace lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как тебя зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}, сегодня {DateTime.Now}");
+            Console.ReadKey();
+        }
+    }
+}
