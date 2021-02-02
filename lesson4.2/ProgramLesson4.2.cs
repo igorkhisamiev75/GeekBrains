b83e181b84c48c7e04d794661cb09880fa13d835
igorkhisamiev75 <khisamievigor@gmail.com>
2021-02-02 11:50:54 +0300

    lesson4

---
+++ lesson4.2/ProgramLesson4.2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace lesson4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числа для получения суммы этих чисел:");
+            string str = Console.ReadLine();
+
+            string[] num = str.Split(' ');
+            int i = 1;
+
+            foreach (var num1 in num)
+            {
+               Console.WriteLine($"{i} число =  {num1} ");
+                i ++;
+            }
+
+            int sum = 0;
+            i = i - 1;
+
+            for (int j=0; j<i; j++)
+            {
+                sum = sum + Convert.ToInt32(num[j]);
+            }
+
+
+            Console.WriteLine($"Сумма чисел равна {sum}");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ lesson4.4/ProgramLesson4.4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lesson4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите порядковый номер последовательности Фибоначи:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            static int Fibonachi(int n)
+            {
+                if (n == 0 || n == 1)
+                {
+                    return n;
+                }
+                else
+                {
+                    return Fibonachi(n - 1) + Fibonachi(n - 2);
+                }
+            }
+
+            Console.WriteLine($"Фибоеаччи {n} = {Fibonachi(n)}");
+            Console.ReadKey();
+
+        }
+    }
+}
