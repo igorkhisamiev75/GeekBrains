4d694569149665e48989fa9af8e7ce9d62a8501d
igorkhisamiev75 <khisamievigor@gmail.com>
2021-02-08 12:00:37 +0300

    lesson5.4

---
+++ lesson5.4/lesson5.4.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace lesson5._4
+{
+    class Program
+
+    {
+        /*(*) Сохранить дерево каталогов и файлов по заданному пути в текстовый файл — с рекурсией и без.*/
+
+        static void Main(string[] args)
+        {
+            //C рекурсией
+            string path = @"C:\Первая";
+            //string fileName = "strartup.txt";
+
+
+            static string[] Dir(string path2) //метод Dir принимает путь, возвращает папки и колличество папок
+            {
+                
+                string[] allfiles2 = Directory.GetDirectories(path2); /* получаем массив всех папок в папке пути*/
+                int i = allfiles2.Length; //получение длины массива
+
+                for (int j = 0; j <= i - 1; j++)
+                {
+                    Console.Write($"{path2}\n"); //выводим путь
+                    
+                    Dir(allfiles2[j]);
+                    Console.Write($"\t{allfiles2[j]}\n");
+
+                }
+                
+                return allfiles2;
+            }
+
+            Dir(path);
+
+         }
+
+       
+    }
+}
